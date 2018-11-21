1ae28543b1a22fb13c126e50467552e745e1569c
adioshun <adioshun@gmail.com>
2018-11-21 03:59:24 -0800

    2018

--- scripts/generate_book.py
+++ scripts/generate_book.py
@@ -1,7 +1,7 @@
 # encoding=utf8
 import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
+#reload(sys)
+#sys.setdefaultencoding('utf8')
 from subprocess import check_call
 import os
 import os.path as op
